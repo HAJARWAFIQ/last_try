6bf1d936e06acf18320a6a7ef154a142790c8c89
Hajar wafiq <wafiqhajaar@gmail.com>
2024-05-22 03:50:10 +0100

    first commit

---
+++ app/src/main/java/com/example/tp_actions/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.example.tp_actions
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.tp_actions.ui.theme.TP_ACTIONSTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TP_ACTIONSTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(nom: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "BONSOIR $nom!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true )
+@Composable
+fun GreetingPreview() {
+    TP_ACTIONSTheme {
+        Greeting("Android")
+    }
+}
---
+++ app/src/test/java/com/example/tp_actions/ExampleUnitTest.kt
@@ -0,0 +1,13 @@
+package com.example.tp_actions
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(5, 2 + 3)
+    }
+}
